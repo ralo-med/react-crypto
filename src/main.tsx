85532b4211d10a98de6425b6e6dbb00e6a72ab4d
Ralo <74175604+ralo-med@users.noreply.github.com>
2025-07-09 08:31:11 +0900

    #4.2 CreateBrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { RouterProvider } from "react-router-dom";
+import router from "./screens/Router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
