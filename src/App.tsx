837dc3ad1f47c92a1e6b542fd3de56000b755417
Ralo <74175604+ralo-med@users.noreply.github.com>
2025-07-09 08:13:36 +0900

    #4.1 BrowserRouter

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Router from "./screens/Router";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Crypto App</h1>
+      <Router />
+    </div>
+  );
+}
+
+export default App;
