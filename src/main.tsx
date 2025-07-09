55993cd4ca8c87847a7d4d1557c55cb22882a0f4
Ralo <74175604+ralo-med@users.noreply.github.com>
2025-07-09 15:20:53 +0900

    #React Query part one

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./screens/Router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
--- src/screens/Coins.tsx
+++ src/screens/Coins.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
 import styled from "styled-components";
+import { fetchCoins } from "../api";
+import { useQuery } from "@tanstack/react-query";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -15,7 +16,11 @@ const Header = styled.header`
   align-items: center;
 `;
 
-const CoinsList = styled.ul``;
+const CoinsList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const Title = styled.h1`
   font-size: 48px;
@@ -34,14 +39,14 @@ const Coin = styled.li`
   background-color: white;
   color: ${(props) => props.theme.colors.bgColor};
   margin-bottom: 10px;
-
   border-radius: 15px;
+  width: 100%;
+
   a {
     padding: 20px;
-    display: block;
-    transition: color 0.2s ease-in;
     display: flex;
     align-items: center;
+    transition: color 0.2s ease-in;
   }
   &:hover {
     color: ${(props) => props.theme.colors.accentColor};
@@ -64,23 +69,18 @@ interface Coin {
 }
 
 function Coins() {
-  const [coins, setCoins] = useState<Coin[]>([]);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    (async () => {
-      const response = await fetch("https://api.coinpaprika.com/v1/coins");
-      const json = await response.json();
-      setCoins(json.slice(0, 100));
-      setLoading(false);
-    })();
-  }, []);
+  const { isLoading, data } = useQuery({
+    queryKey: ["allCoins"],
+    queryFn: fetchCoins,
+  });
+
   return (
     <Container>
       <Header>
         <Title>Coins</Title>
       </Header>
       <CoinsList>
-        {coins.map((coin) => (
+        {data?.slice(0, 100).map((coin: Coin) => (
           <Coin key={coin.id}>
             <Link to={`/${coin.id}`}>
               <Img
@@ -91,7 +91,7 @@ function Coins() {
             </Link>
           </Coin>
         ))}
-        {loading ? <Loader>Loading...</Loader> : null}
+        {isLoading ? <Loader>Loading...</Loader> : null}
       </CoinsList>
     </Container>
   );
