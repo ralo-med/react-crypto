32620e1f1b10d287099c26980b614ff3ed864462
Ralo <74175604+ralo-med@users.noreply.github.com>
2025-07-09 14:38:34 +0900

    #5.6 DataTypes

--- src/screens/Coin.tsx
+++ src/screens/Coin.tsx
@@ -25,9 +25,66 @@ const Loader = styled.span`
   display: block;
 `;
 
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_15m: number;
+      percent_change_30m: number;
+      percent_change_1h: number;
+      percent_change_6h: number;
+      percent_change_12h: number;
+      percent_change_24h: number;
+      percent_change_7d: number;
+      percent_change_30d: number;
+      percent_change_1y: number;
+      ath_price: number;
+      ath_date: string;
+      percent_from_price_ath: number;
+    };
+  };
+}
+
 function Coin() {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams();
+  const [info, setInfo] = useState<InfoData>();
+  const [price, setPrice] = useState<PriceData>();
   useEffect(() => {
     (async () => {
       const infoData = await (
@@ -38,6 +95,8 @@ function Coin() {
         await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
       ).json();
       console.log(priceData);
+      setInfo(infoData);
+      setPrice(priceData);
       setLoading(false);
     })();
   }, [coinId]);
